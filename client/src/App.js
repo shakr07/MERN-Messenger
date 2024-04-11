b2939fa4c762e817b917b366cf9dcc1457592284
shakr07 <shashanksingh17520@gmail.com>
2024-04-11 16:25:37 +0530

    chatwindow

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ChatWindow from "./components/ChatWindow";
+  import Container from "@mui/material/Container";
+function App() {
+ 
+  return (
+    <div>
+      <Container>
+        <ChatWindow />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/ChatWindow.js
@@ -0,0 +1,113 @@
+    import React from 'react'
+    import Box from "@mui/material/Box";
+    import Typography from "@mui/material/Typography";
+    import Button from "@mui/material/Button";
+    import TextField from "@mui/material/TextField";
+    import InputAdornment from "@mui/material/InputAdornment";
+    import OutlinedInput from "@mui/material/OutlinedInput";
+    import { useEffect, useState } from "react";
+    import IconButton from "@mui/material/IconButton";
+    import { io } from "socket.io-client";
+    import SendIcon from "@mui/icons-material/Send";
+    import Card from "@mui/material/Card";
+    import InputLabel from "@mui/material/InputLabel";
+    function ChatWindow() {
+       const [socket, setSocket] = useState(null);
+       const [message, setMessage] = useState(" ");
+       const [chat, setchat] = useState([""]);     
+      const [typing, setTyping] = useState(false); 
+       useEffect(() => {
+         setSocket(io("http://localhost:4000"));
+       }, []);
+
+       useEffect(() => {
+         if (!socket) return;
+
+         socket.on("message-from-server", (data) => {
+           setchat((prev) => [
+             ...prev,
+             { message: data.message, received: true },
+           ]);
+         });
+
+         socket.on("typing-from-server", () => {
+          setTyping(true);
+         });
+       }, [socket]);
+      const [typingTimeout, settypingTimeout] = useState(null);
+       const handleform = (e) => {
+         e.preventDefault();
+         socket.emit("send-message", { message });
+          setchat((prev) => [
+            ...prev,
+            { message , received: false },
+          ]);
+         setMessage(""); // Clear the message field
+       };
+       const handleInput=(e)=>{
+        setMessage(e.target.value);
+        socket.emit("typing-started");
+        if (typingTimeout) clearTimeout(typingTimeout);
+
+        settypingTimeout(
+          setTimeout(() => {
+            socket.emit("typing-stoped" );
+          }, 1000)
+        );
+       } 
+        return (
+          <>
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+              <Card
+                style={{
+                  padding: 2,
+                  marginTop: 50,
+                  width: "60% ",
+                  backgroundColor: "gray",
+                }}
+              >
+                <Box style={{ marginBottom: 40 }}>
+                  {chat.map((data) => (
+                    //if true then it is receiver so it is on left  and
+                    <Typography
+                      key={data.message}
+                      sx={{ textAlign: data.received ? "left" : "right" }}
+                    >
+                      {data.message}
+                    </Typography>
+                  ))}
+                </Box>
+                <Box component="form" onSubmit={handleform}>
+                  {typing && (
+                    <InputLabel
+                      sx={{ color: "orange " }}
+                      shrink
+                      htmlFor="message-input"
+                    >
+                      Typing...
+                    </InputLabel>
+                  )}
+                  <OutlinedInput
+                    sx={{ backgroundColor: "white", width: "100%  " }}
+                    size="small"
+                    id="message-input"
+                    value={message}
+                    placeholder="Write your message"
+                    inputProps={{ "aria-label": "search" }}
+                    onChange={handleInput}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton type="submit" edge="end">
+                          <SendIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </Box>
+              </Card>
+            </Box>
+          </>
+        );
+    }
+    
+    export default ChatWindow
